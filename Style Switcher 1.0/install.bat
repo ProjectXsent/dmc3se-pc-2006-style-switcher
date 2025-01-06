9e99db6b58e7690d899ed995c295896540b8ae50
ProjectXsent <25782670+ProjectXsent@users.noreply.github.com>
2025-01-06 13:26:39 +0800

    Added Style Switcher 1.0 files

---
+++ Style Switcher 1.0/install.bat
@@ -0,0 +1,18 @@
+@echo off
+
+cd /d "%~dp0"
+
+set XDELTA=
+
+move "data\GData.pak" "new_GData.pak"
+
+xdelta3-3.0.11-x86_64.exe -d -s "new_GData.pak" "snd.xdelta" "GDATA.AFS"
+echo.
+echo Please wait for a few seconds for the GDATA.AFS patch to apply...
+timeout 10 >nul
+
+move "new_GData.pak" "data\GData.pak"
+
+del "snd.xdelta"
+del "xdelta3-3.0.11-x86_64.exe"
+del "install.bat"
