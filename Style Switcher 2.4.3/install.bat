747cbcb74f0e12080f03b20e7534d3fce6811315
ProjectXsent <25782670+ProjectXsent@users.noreply.github.com>
2025-01-09 17:10:20 +0800

    Add files via upload

--- Style Switcher 2.4.3/install.bat
+++ Style Switcher 2.4.3/install.bat
@@ -5,12 +5,13 @@ cd /d "%~dp0"
 set XDELTA=
 
 move "data\GData.pak" "new_GData.pak"
+ren "DMC3SE.exe" "dmc3se.exe"
 
 xdelta3-3.0.11-x86_64.exe -d -s "new_GData.pak" "snd.xdelta" "GDATA.AFS"
 echo.
 echo Please wait for a few seconds for the GDATA.AFS patch to apply...
 timeout 10 >nul
-echo Press any key to exit.
+echo Press any key to exit...
 pause >nul
 
 move "new_GData.pak" "data\GData.pak"
