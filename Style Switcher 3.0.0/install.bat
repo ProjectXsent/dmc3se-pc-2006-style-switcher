ac8cdad5e9c999ef71455fa947d53cfa4dcb464d
ProjectXsent <25782670+ProjectXsent@users.noreply.github.com>
2025-01-09 18:21:40 +0800

    Add files via upload

--- Style Switcher 3.0.0/install.bat
+++ Style Switcher 3.0.0/install.bat
@@ -25,7 +25,7 @@ for /f "delims=" %%a in ('dir /A /B') do (
 	if /I not "%%a"=="snd.drv" (
 	if /I not "%%a"=="StyleSwitcher.dll" (
 	if /I not "%%a"=="StyleSwitcher.ini" (
-	if /I not "%%a"=="readme.txt" (
+	if /I not "%%a"=="Style Switcher 3 readme.txt" (
 	if /I not "%%a"=="uninstall.bat" (
 	if /I not "%%a"=="virtual_key_codes.html" (
 	if /I not "%%a"=="XInput1_3.dll" (
@@ -61,6 +61,7 @@ for /f "delims=" %%a in ('dir /A /B') do (
 
 move "data\GData.pak" "GDATA.AFS"
 move "data" "_Restore\data"
+ren "DMC3SE.exe" "dmc3se.exe"
 
 md "native"
 
@@ -72,6 +73,29 @@ ren "native\movie\ipu\*.bin" "*.bmp"
 
 ren "native\sound\*.bin" "*.ogg"
 
-md "native\script"
+:select
+cls
 
+echo.
+echo Select loader.
+echo.
+echo 0. dinput8.dll for Keyboard Support
+echo 1. dsound.dll for XInput/Xbox Controller Support (requires XInput Plus)
+echo.
+
+set /p loader="Select #"
+
+if "%loader%"=="0" goto 0
+if "%loader%"=="1" goto 1
+
+goto select
+
+:0
+del "dsound.dll"
+goto end
+
+:1
+del "dinput8.dll"
+
+:end
 del "install.bat"
